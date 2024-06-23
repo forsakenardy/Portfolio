17a7ff4a08c2a2dd61b791da3101bcc329ff6cce
Nardy <nardy.allstars@gmail.com>
2024-06-23 22:05:59 +0200

    mi primer intento

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector('.photo').addEventListener('animationend', function() {
+    this.classList.add('show-my-photo');
+});
+document.querySelector('.photo').addEventListener('animationend', function() {
+    this.classList.add('show-contact-info');
+});
